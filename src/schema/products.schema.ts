9efe53612d9333ec935b85c5f5e0436ba4aeba2c
Rit0106 <ritikaraj6202088782@gmail.com>
2025-09-30 13:49:16 +0530

    Add files via upload

---
+++ src/schema/products.schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const productsSchema = z.object({
+	name: z.string(),
+	sku: z.string().toUpperCase(),
+	restockLevel: z.number(),
+	warehouseId: z.number(),
+});
+
+export type productstype = z.infer<typeof productsSchema>;
